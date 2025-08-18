844e974a47babf27294516f02fd9ca79ba5ed2c2
devi5040 <dpraidola@gmail.com>
2025-08-18 13:11:31 +0530

    fix: add routes to index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,21 @@
 // };
 // api/index.js
 const express = require("express");
+
+const routes = require("./routes/generateTest");
+const path = require("path");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+const PORT = process.env.PORT || 5050;
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 app.get("/integration-test", (req, res) => {
   res.json({ message: "Integration test working 🚀" });
