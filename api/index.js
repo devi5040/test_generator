38ec95d7086f63b9c8bac4bf9dbac527472b27b5
devi5040 <dpraidola@gmail.com>
2025-08-18 13:21:14 +0530

    fix: fixed the folder structure

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(routes);
 
 app.get("/integration-test", (req, res) => {
