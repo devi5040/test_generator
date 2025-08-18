e0a87d4d755222bd486fb214b027d5d09227011e
devi5040 <dpraidola@gmail.com>
2025-08-18 13:08:48 +0530

    fix: setup vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
-const serverless = require("serverless-http");
-const app = require("../app"); // your existing Express app
+// const serverless = require("serverless-http");
+// const app = require("../app"); // your existing Express app
 
-module.exports = (req, res) => {
-  const handler = serverless(app);
-  return handler(req, res);
-};
+// module.exports = (req, res) => {
+//   const handler = serverless(app);
+//   return handler(req, res);
+// };
+// api/index.js
+const express = require("express");
+const app = express();
+
+app.get("/integration-test", (req, res) => {
+  res.json({ message: "Integration test working 🚀" });
+});
+
+// Export Express app directly
+module.exports = app;
